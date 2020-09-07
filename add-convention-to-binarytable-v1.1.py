7b920c9d82f639c51d0f6c28c4e633b6569d34e0
aniakpinar <56962832+aniakpinar@users.noreply.github.com>
2020-09-07 22:39:10 +0300

    Update add-convention-to-binarytable-v1.1.py

--- add-convention-to-binarytable-v1.1.py
+++ add-convention-to-binarytable-v1.1.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # coding=utf-8
 #
-# C.D. Dunn†, B.A. Akpınar, and V. Sharma†. An unusual amino acid substitution within the hummingbird
-# cytochrome c oxidase alters a key proton-conducting channel. bioRxiv. doi: 10.1101/610915.
+# C.D. Dunn†, B.A. Akpınar, and V. Sharma†. 2019. An unusual amino acid substitution within the hummingbird
+# cytochrome c oxidase alters a key proton-conducting channel. G3: Genes, Genomes, Genetics, 10:7, 2477-2485
+# doi: 10.1534/g3.120.401312
 
 # This script adds amino acid position [within the full length protein] correspondences of the alignment positions
 # based on a user-defined species.
